5acd29d7982bce86deb01ea770edf21d612301be
Itai <itai.nulman@campus.technion.ac.il>
2023-05-18 17:34:16 +0300

    0/0

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+int bitAnd(int x, int y);
+
 int main() {
-    printf("Hello, World!\n");
+int x = 9;
+int y = 3;
+int z = bitAnd(x, y);
+printf("z = %d\n", z);
+
     return 0;
 }
+
+
+
+/*
+* bitAnd ‐ x&y using only ~ and |
+* Example: bitAnd(6, 5) = 4
+* Legal ops: ~ |
+* Max ops: 8
+* Rating: 1
+*/
+int bitAnd(int x , int y) {
+    return ~(~x | ~y);
+}
+

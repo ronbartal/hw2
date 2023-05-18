21146c78540e56cc8f8dc619d454aa8d4a987b8c
Itai <itai.nulman@campus.technion.ac.il>
2023-05-18 17:35:21 +0300

    0/0

---
+++ cmake-build-debug/CMakeFiles/hw2.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/לימודים/סמסטר א/מבוא למדמח/הכנה למבחן מדמח/מועד ב/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/לימודים/סמסטר א/מבוא למדמח/הכנה למבחן מדמח/מועד ב/hw2/main.c" "CMakeFiles/hw2.dir/main.c.o" "gcc" "CMakeFiles/hw2.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/main.c.o"
+  "CMakeFiles/hw2.dir/main.c.o.d"
+  "hw2"
+  "hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
